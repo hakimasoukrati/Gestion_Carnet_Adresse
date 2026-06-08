ca4c36a7b27351f944ed75a902364e44bc50de12
hakimasoukrati <hakimasoukrati.h@gmail.com>
2026-06-08 17:12:05 +0000

    CD-48: feat: formulaire ajout contact web

--- flask_contacts/app.py
+++ flask_contacts/app.py
@@ -114,3 +114,4 @@ if __name__ == "__main__":
 # CD-41: Confirmation JavaScript avant suppression 
 # CD-45: Initialisation app Flask 
 # CD-46: Route / page d'accueil 
+# CD-48: Route POST /ajouter formulaire 
